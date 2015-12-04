3cb6dd9629890c7c6ea8ae534a8196feb2922b01
bgeetika <geetikagarg07@gmail.com>
2015-12-03 23:34:07 -0800

    adding feature of decoding random image

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -6,16 +6,73 @@ import urllib2
 import urllib
 
 import json
-
+import base64
 
 class  MainPageJquery(webapp2.RequestHandler):
   def get(self):
     self.response.out.write(
 		    """
 		    <html>
-                    <head><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+                    <head>
+		    <H1 align ="center">
+		    <u align="center">
+		    NEURAL NET CAPTCHA CRACKER
+		    </u>
+		    </H1>
+		    <h3 align="center">
+		    Project by: Geetika Garg
+                    </h3>
+		    <h3 align="center">
+		    Project Advisor: Dr. Chris Pollett
+		    <br><br>
+                    </h3>
+                    <div id="footer">
+		    <ul>
+				<p style="position: fixed; bottom: 0; width:100%; text-align: center">
+				<a href="https://github.com/bgeetika/Captcha-Decoder">Project Github Link
+				</a>
+				</p>
+	            </ul></div>
+		    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
                     </head>
                     <body>
+
+                    <form id = "generate" action ="/generate" method="get">
+		    <input type ="submit" id = "sub" value="Generate and Decode any CAPTCHA"></input>
+                    
+		    </form>
+                    <img id="res_image"/> 
+                    <div id = "result_image"></div>
+
+                    <script>
+                    // Return decoded captcha with image
+                    $( "#generate" ).submit(function( event ) {
+                       console.log("generate");                    
+                      // Stop form from submitting normally
+                      event.preventDefault();
+                      // Get some values from elements on the page:
+                      url = $( this ).attr( "action" );
+                        // Send the data using post
+                        var posting = $.get(url);
+                        // Put the results in a div
+                        posting.done(function( data ) {
+                          console.log(data);
+                          var content = data["file_content"];
+                          console.log(typeof(content));
+                          var res = $( data ).find( "#answer" );
+                          document.getElementById("res_image").src  = 'data:image/jpeg;base64,' + content;
+                          $( "#result_image" ).empty().append("The CAPTCHA decodes to : " + data['answer'] );
+                          $('#submit').hide();
+                          $ ('#src').hide();
+                          $('#model').hide();
+                          var paragraph =  $('#instruct');
+                          paragraph.hide();                      
+                      });
+                    });
+                    </script>
+
+                
+		    
 		    <form id="searchForm" enctype="multipart/form-data"  action="/submit_photo" method="post">
 		    <p id="instruct" > Please upload an image-based CAPTCHA
 		    </p>
@@ -33,6 +90,9 @@ class  MainPageJquery(webapp2.RequestHandler):
                     </form>
                     <div id="result">
 		     </div>
+
+                    
+
                     <script>
                       function showImage(src,target) {
                         var fr=new FileReader();
@@ -59,7 +119,7 @@ class  MainPageJquery(webapp2.RequestHandler):
 					          });
 						      });
 		    </script>
-                   
+
 
                     <script>
                     // Attach a submit handler to the form
@@ -125,9 +185,23 @@ class HandlePhoto(webapp2.RequestHandler):
   }          
              print obj
  	     self.response.out.write(json.dumps(obj))
+
+
+     def get(self):
+	     headers = {'content-type': 'application/json'}
+	     method = "GET"
+	     url = "http://104.196.6.225:8080/"
+	     req = urllib2.Request(url=url)
+	     req.get_method = lambda: method
+	     req.add_header('Content-Type', 'application/json')
+	     f = urllib2.urlopen(req)
+	     the_page_res = f.read()
+             print 'length_of_json_string:', len(the_page_res), the_page_res[:50]
+             self.response.headers['Content-Type'] = 'application/json'
+             self.response.out.write(the_page_res)
              
 
 
 
 app = webapp2.WSGIApplication([
-    ('/submit_photo',HandlePhoto),('/', MainPageJquery),], debug=True)
+    ('/submit_photo',HandlePhoto),('/', MainPageJquery),('/generate', HandlePhoto),], debug=True)
